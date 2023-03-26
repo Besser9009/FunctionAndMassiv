ce52dca835bc57955e2388da168d79d28afa49b0
besser <besser9009@mail.ru>
2023-03-27 02:57:35 +0300

    add 3 file: Examp009, Examp010, Examp011

--- Examp008_IntroMax/Program.cs
+++ Examp008_IntroMax/Program.cs
@@ -15,11 +15,7 @@ int b2 = 52;
 int c2 = 0;
 int d2 = -7;
 int a3 = 90;
-//int max1 = Max(a1, b1, c1);
-//int max2 = Max(d1, a2, b2);
-//int max3 = Max(c2, d2, a3);
 int max = Max(Max(a1, b1, c1), Max(d1, a2, b2), Max(c2, d2, a3));
-
 Console.Write("max = ");
 Console.WriteLine(max);
 
---
+++ Examp010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {12, 165, 120, 48, 16, 0, 67984, 167, -4946, 16};
+
+int n = array.Length;
+int find = 16;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        Console.WriteLine("End.");
+        break;
+    }
+    index++;
+}
