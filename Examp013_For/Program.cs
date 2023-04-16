aab38f39237cb1ab8cf1c62cdc660b16da1a6aac
besser <besser9009@mail.ru>
2023-04-16 19:42:42 +0300

    add For

---
+++ Examp013_For/Program.cs
@@ -0,0 +1,9 @@
+﻿string Method4(int count, string text){
+    string result = String.Empty;
+    for (int i = 0; i < count; i++){
+        result += text;
+    }
+    return result;
+}
+string res = Method4(10, "Возможен такой метод ");
+System.Console.WriteLine(res);
