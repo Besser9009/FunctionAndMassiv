776b0019e1e250a13ab1098ba446b8369f2aa6b7
besser <besser9009@mail.ru>
2023-04-16 19:43:37 +0300

    add Pract

---
+++ Examp015_Pract/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+string text = "- Я думая, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винцегероде, "
+            + "вы бы взяли приступом Согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result += $"{newValue}";
+        else result += $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, 'к', 'К');
+System.Console.WriteLine(newText);
+newText = Replace(text, ' ', '_');
+System.Console.WriteLine(newText);
+newText = Replace(text, 'С', 'с');
+System.Console.WriteLine(newText);
